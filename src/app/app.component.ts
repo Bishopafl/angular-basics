a77f01129c833790d3936877c88bf2eaca001be5
Lopez Family <lopezfamily@lopezs-air.lan>
2022-07-22 14:18:57 -0400

    Understanding the basics of building angular apps. Just getting started

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,13 @@
-import { Component } from '@angular/core';
+import { Component } from "@angular/core";
 
 @Component({
   selector: 'app-root',
+  // template: `<p>Ayo, take me out to dinner</p>`,
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
+  // styles: ['']
 })
+
 export class AppComponent {
-  title = 'basics';
+
 }
--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,23 @@
 import { enableProdMode } from '@angular/core';
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
-
 import { AppModule } from './app/app.module';
 import { environment } from './environments/environment';
 
+/**
+ * This will allow us to determine if we are in production or not
+ * Initializing before the app is loaded
+ */
 if (environment.production) {
-  enableProdMode();
+    enableProdMode();
 }
 
+/**
+ * 
+ * #Ahead-Of-Time Complilation: 
+ * 
+ * platformBrowserDynamic function will load an object with information on the current platform, in this case, the platform is the browser.
+ * 
+ * Then we take the AppModule and import it into a bootstrapModule function which is chained off our inital function. The boostrapModule will act as the glue that will build the app while logging any errors that would come about during compliation
+ */
 platformBrowserDynamic().bootstrapModule(AppModule)
-  .catch(err => console.error(err));
+    .catch(err => console.error(err));
