bf6b82ed28f4e2b3ee1984559cb8f04de5956d9b
Lopez Family <lopezfamily@lopezs-air.lan>
2022-07-28 23:53:29 -0400

    Creating components that will contain other pieces of code

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,15 @@
 import { NgModule } from "@angular/core";// adds meta data to class that lets angular know this is a module
 import { BrowserModule } from "@angular/platform-browser";
 import { AppComponent } from "./app.component";
+import { PostComponent } from './post/post.component';
 
 @NgModule({
   imports: [
     BrowserModule,
   ],
   declarations: [
-    AppComponent
+    AppComponent,
+    PostComponent
   ],
   bootstrap: [AppComponent]
 })
