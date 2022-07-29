43dd11f47ee64efbfb167b3d5d2894883829080f
Lopez Family <lopezfamily@lopezs-air.lan>
2022-07-29 13:02:13 -0400

    Input aliases and event emitters within angular, learning the ins and outs of sending data from parent to child and vice versa

--- src/app/post/post.component.ts
+++ src/app/post/post.component.ts
@@ -1,4 +1,17 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, EventEmitter, Output } from '@angular/core';
+/**
+ * By creating a event emitter, we can send data via the events on the browser
+ * And apply a string generic to the event 
+ * 
+ * Events are a binary way to send data from parent to child
+ * data emitted from an event is stored within the $event var
+ * 
+ * :19 is using an alias for input of post image, the angular 
+ * docs say to avoid using aliases on inputs and output.
+ * 
+ * 
+ * 
+ */
 
 @Component({
   selector: 'app-post',
@@ -7,4 +20,6 @@ import { Component, Input } from '@angular/core';
 })
 export class PostComponent {
   @Input('img') postImg = ''
+  @Output() imgSelected = new EventEmitter<string>() // this creates our event to emit
 }
+ 
